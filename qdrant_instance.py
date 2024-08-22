fc79c255d6d00d059853c1586654e52bdd3ca701
Mourad <Mourad@DESKTOP-HLIDCNM>
2024-08-23 00:14:24 +0100

    add range price on search product

--- qdrant_instance.py
+++ qdrant_instance.py
@@ -4,32 +4,35 @@ from qdrant_client.http import models
 
 config = dotenv_values(".env")
 
-
 qdrant_client = QdrantClient(
     url=config.get("QDRANT_URL"),
     api_key=config.get("QDRANT_API_KEY"),
 )
 
+
 # Create or recreate a collection (only needs to be done once)
 def create_collection():
     # if not qdrant_client.collection_exists(config.get('PRODUCT_COLLECTION')):
-        qdrant_client.recreate_collection(
-            collection_name=config.get('PRODUCT_COLLECTION'),
-            vectors_config=models.VectorParams(size=config.get('VECTOR_SIZE'), distance=models.Distance[config.get('DISTANCE_METRIC').upper()]),
-        )
-        qdrant_client.recreate_collection(
-            collection_name=config.get('SELLER_COLLECTION'),
-            vectors_config=models.VectorParams(size=config.get('VECTOR_SIZE'),
-                                               distance=models.Distance[config.get('DISTANCE_METRIC').upper()]),
-        )
-        qdrant_client.recreate_collection(
-            collection_name=config.get('CRAWEL_TIME_COLLECTION'),
-            vectors_config=models.VectorParams(size=config.get('VECTOR_SIZE'),
-                                               distance=models.Distance[config.get('DISTANCE_METRIC').upper()]),
-        )
+    qdrant_client.create_collection(
+        collection_name=config.get('PRODUCT_COLLECTION'),
+        vectors_config=models.VectorParams(size=config.get('VECTOR_SIZE'),
+                                           distance=models.Distance[config.get('DISTANCE_METRIC').upper()]),
+    )
+    qdrant_client.create_collection(
+        collection_name=config.get('SELLER_COLLECTION'),
+        vectors_config=models.VectorParams(size=config.get('VECTOR_SIZE'),
+                                           distance=models.Distance[config.get('DISTANCE_METRIC').upper()]),
+    )
+    qdrant_client.create_collection(
+        collection_name=config.get('CRAWEL_TIME_COLLECTION'),
+        vectors_config=models.VectorParams(size=config.get('VECTOR_SIZE'),
+                                           distance=models.Distance[config.get('DISTANCE_METRIC').upper()]),
+    )
+
+
 def create_collection_catagories():
-        qdrant_client.recreate_collection(
-            collection_name=config.get('CATAGORIES_COLLECTION'),
-            vectors_config=models.VectorParams(size=config.get('VECTOR_SIZE'),
-                                               distance=models.Distance[config.get('DISTANCE_METRIC').upper()]),
-        ) 
+    qdrant_client.create_collection(
+        collection_name=config.get('CATAGORIES_COLLECTION'),
+        vectors_config=models.VectorParams(size=config.get('VECTOR_SIZE'),
+                                           distance=models.Distance[config.get('DISTANCE_METRIC').upper()]),
+    )
